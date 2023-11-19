93959e4be023c724f5181f80f5bb075c9627da9d
kevinstaresdarbon <red.inshou@gmail.com>
2023-11-19 22:26:26 +0000

    adds logic to improve looking up cities and geo-coding the lookup, now with a search history of saved search terms

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,32 +1,32 @@
-function renderLookup(arr){
+function renderLookup(arr) {
 
     var $locations = $('#locations');
-        $locations.empty();
+    $locations.empty();
 
-    for (var i=0; i < arr.length; i++){
+    for (var i = 0; i < arr.length; i++) {
 
         var locationStr = "";
 
-        if (arr[i].name){
+        if (arr[i].name) {
             locationStr += arr[i].name;
         }
 
-        if (arr[i].state){
+        if (arr[i].state) {
             locationStr += " " + arr[i].state;
         }
 
-        if (arr[i].country){
+        if (arr[i].country) {
             locationStr += " " + arr[i].country;
         }
 
-        var template =  $('<div class="btn btn-secondary location-btn my-1 w-100" data-lon="' + arr[i].lon +'"data-lat="' + arr[i].lat + '">' + locationStr + '</div>');
+        var template = $('<div class="btn btn-secondary location-btn isInLookup my-1 w-100 h-3r" data-lon="' + arr[i].lon + '"data-lat="' + arr[i].lat + '"  data-text="' + locationStr + '">' + locationStr + '</div>');
 
         $locations.append(template);
     }
 }
 
 
-function handleLookup(event){
+function handleLookup(event) {
 
     event.preventDefault();
 
@@ -44,16 +44,35 @@ function handleLookup(event){
 
 }
 
-function handleSearch(event){
-    
+function handleSearch(event) {
+
     event.preventDefault();
 
     $clicked = $(event.target);
-
+    $history = $('#history');
     var lat = $clicked.attr("data-lat");
     var lon = $clicked.attr("data-lon");
+    var text = $clicked.attr("data-text");
 
-    if(lat&&lon){
+
+    if ($clicked.hasClass('isInLookup')) {
+        $clicked.removeClass('isInLookup');
+        var historyStr = retrieveHistory();
+        var historyArr = JSON.parse(historyStr);
+ 
+        historyArr[historyArr.length] = {text: text, lat: lat, lon: lon}
+
+        localStorage.setItem('button-history', JSON.stringify(historyArr));
+
+        $history.empty();
+        renderHistory(historyArr, $('#history'));
+    }
+
+    var $locations = $('#locations');
+    $locations.empty();
+
+
+    if (lat && lon) {
         var queryURICurrent = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
 
         var queryURIFiveDay = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
@@ -65,7 +84,7 @@ function handleSearch(event){
             })
             .catch((err) => console.log("An error occured while fetching the current data: " + err));
 
-            fetch(queryURIFiveDay)
+        fetch(queryURIFiveDay)
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
@@ -74,5 +93,30 @@ function handleSearch(event){
     }
 }
 
+function retrieveHistory() {
+
+    var historyStr = localStorage.getItem('button-history');
+
+    if (!historyStr) {
+        localStorage.setItem('button-history', JSON.stringify([""]));
+        return JSON.stringify([""]);
+    } else {
+        return historyStr
+    }
+
+}
+
+function renderHistory(arr, element){
+    for(let i=0; i < arr.length; i++){
+        if (arr[i].lat){
+            var button = $('<div class="btn btn-secondary location-btn my-1 w-100 h-3r" data-lon="' + arr[i].lon + '"data-lat="' + arr[i].lat + '">' + arr[i].text + '</div>');
+            element.prepend(button);
+        }
+    }
+}
+
+
 $('#lookup-button').on("click", handleLookup);
-$('#locations').on("click", '.location-btn', handleSearch);
+$(document).on("click", '.location-btn', handleSearch);
+
+renderHistory(JSON.parse(retrieveHistory()), $('#history'));
