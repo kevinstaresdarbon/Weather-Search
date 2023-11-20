96d3350578a2a97ca2f3dfb900b3df5c878cc358
kevinstaresdarbon <red.inshou@gmail.com>
2023-11-20 14:41:52 +0000

    adds jQuery logic to display a card for the current weather conditions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,19 @@
+
+async function cardMaker(location, temp, feelsLike, humidity, windSpeed, windDir, iconcode, $element) {
+
+    var $icon = $('<div id="icon" class="card-image-top"><img id="wicon" src="http://openweathermap.org/img/w/' + iconcode + '.png" alt="Weather icon"></img></div>');
+
+    var cardTemplate = ('<section class="card text-bg-light"> <div class="card-body card-content"> </div> </section>');
+    $element.append(cardTemplate);
+    $('.card').prepend($icon);
+    $('.card-content').append($('<div class="card-title fs-5">' + location + '</div>'));
+    $('.card-content').append($('<div class="card-title card-temp fs-5">' + "Temperature: "+ temp + " celsius" + '</div>'));
+    $('.card-content').append($('<div class="card-title card-temp fs-5">' + "Feels like: " + feelsLike + " celsius" + '</div>'));
+    $('.card-content').append($('<div class="card-title card-humidity fs-5">' + "Humidity: " + humidity + '</div>'));
+    $('.card-content').append($('<div class="card-title card-wind fs-5">' + "Wind Speed: " + windSpeed +'</div>'));
+    $('.card-content').append($('<div class="card-title card-wind fs-5">' + "Wind Directon: " + windDir + " degrees" + '</div>'));
+}
+
 function renderLookup(arr) {
 
     var $locations = $('#locations');
@@ -44,7 +60,7 @@ function handleLookup(event) {
 
 }
 
-function handleSearch(event) {
+async function handleSearch(event) {
 
     event.preventDefault();
 
@@ -59,28 +75,30 @@ function handleSearch(event) {
         $clicked.removeClass('isInLookup');
         var historyStr = retrieveHistory();
         var historyArr = JSON.parse(historyStr);
- 
-        historyArr[historyArr.length] = {text: text, lat: lat, lon: lon}
+
+        historyArr[historyArr.length] = { text: text, lat: lat, lon: lon }
 
         localStorage.setItem('button-history', JSON.stringify(historyArr));
 
         $history.empty();
         renderHistory(historyArr, $('#history'));
-    }
 
-    var $locations = $('#locations');
-    $locations.empty();
+        var $locations = $('#locations');
+        $locations.empty();
+    }
 
 
     if (lat && lon) {
-        var queryURICurrent = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
+        var queryURICurrent = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY + "&units=metric";
 
-        var queryURIFiveDay = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY;
+        var queryURIFiveDay = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY + "&units=metric";
 
         fetch(queryURICurrent)
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
+                $('#today').empty();
+                cardMaker(data.name, data.main.temp, data.main.feels_like, data.main.humidity, data.wind.speed, data.wind.deg, data.weather[0].icon, $('#today'));
             })
             .catch((err) => console.log("An error occured while fetching the current data: " + err));
 
@@ -106,9 +124,9 @@ function retrieveHistory() {
 
 }
 
-function renderHistory(arr, element){
-    for(let i=0; i < arr.length; i++){
-        if (arr[i].lat){
+function renderHistory(arr, element) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].lat) {
             var button = $('<div class="btn btn-secondary location-btn my-1 w-100 h-3r" data-lon="' + arr[i].lon + '"data-lat="' + arr[i].lat + '">' + arr[i].text + '</div>');
             element.prepend(button);
         }
